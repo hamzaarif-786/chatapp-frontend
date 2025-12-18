1214dadc08055dcc865794140e6831f48207f263
hamzaarif-786 <arifhamza186@gmail.com>
2025-12-18 21:35:01 +0500

    Update server URL to production endpoint

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import {Provider} from "react-redux"
 import { store } from './redux/store.js'
-export const serverUrl="http://localhost:8000"
+export const serverUrl="chatapp-backend-ashen-iota.vercel.app"
 createRoot(document.getElementById('root')).render(
 <BrowserRouter>
 <Provider store={store}>
