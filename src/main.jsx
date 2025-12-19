d197cdd6821b2df678d6af94f36c455a2905ee10
hamzaarif-786 <arifhamza186@gmail.com>
2025-12-19 14:53:10 +0500

    Refactor main.jsx to include StrictMode and update URL
    Updated the backend server URL and wrapped the render method in StrictMode.

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import {Provider} from "react-redux"
-import { store } from './redux/store.js'
-export const serverUrl="chatapp-backend-ashen-iota.vercel.app"
-createRoot(document.getElementById('root')).render(
-<BrowserRouter>
-<Provider store={store}>
-    <App />
-    </Provider>
-</BrowserRouter>
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux";
+import { store } from './redux/store.js';
 
-)
+// Backend server URL
+export const serverUrl = "https://chatapp-backend-ashen-iota.vercel.app/api";
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </StrictMode>
+);
